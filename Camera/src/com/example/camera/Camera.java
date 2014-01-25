d9100569bec9589c56bd3be5dfae7956a8f085d8
junweizhang23 <junweizhang23@gmail.com>
2014-01-25 15:55:37 -0500

    pro camera

--- Camera/src/com/example/camera/Camera.java
+++ Camera/src/com/example/camera/Camera.java
@@ -2,14 +2,36 @@ package com.example.camera;
 
 import android.os.Bundle;
 import android.app.Activity;
+import android.content.Intent;
+import android.graphics.Bitmap;
 import android.view.Menu;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.ImageView;
 
-public class Camera extends Activity {
+public class Camera extends Activity implements View.OnClickListener {
+
+	ImageButton ib;
+	Button b;
+	ImageView iv;
+	Intent i;
+	final static int cameraData = 0;
+	Bitmap bmp;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_camera);
+		initialize();
+	}
+
+	private void initialize() {
+		iv = (ImageView) findViewById(R.id.Capture);
+		ib = (ImageButton) findViewById(R.id.bSetWall);
+
+		iv.setOnClickListener(this);
+		ib.setOnClickListener(this);
 	}
 
 	@Override
@@ -17,6 +39,44 @@ public class Camera extends Activity {
 		// Inflate the menu; this adds items to the action bar if it is present.
 		getMenuInflater().inflate(R.menu.camera, menu);
 		return true;
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see android.app.Activity#onActivityResult(int, int,
+	 * android.content.Intent)
+	 */
+	@Override
+	protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+		// TODO Auto-generated method stub
+		super.onActivityResult(requestCode, resultCode, data);
+
+		if (resultCode == RESULT_OK) {
+			Bundle extras = data.getExtras();
+			bmp = (Bitmap) extras.get("data");
+
+			iv.setImageBitmap(bmp);
+
+		}
+
+	}
+
+	@Override
+	public void onClick(View v) {
+		// TODO Auto-generated method stub
+		switch (v.getId()) {
+		case R.id.Capture:
+
+			break;
+
+		case R.id.bSetWall:
+
+			i = new Intent(android.provider.MediaStore.ACTION_IMAGE_CAPTURE);
+			startActivityForResult(i, cameraData);
+
+			break;
+		}
 	}
 
 }
