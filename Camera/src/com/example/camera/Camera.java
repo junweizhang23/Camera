ff2d5fbb2dcc76424a489ebaa9457cebf15943ce
junweizhang23 <junweizhang23@gmail.com>
2014-01-25 18:17:33 -0500

    set wall

--- Camera/src/com/example/camera/Camera.java
+++ Camera/src/com/example/camera/Camera.java
@@ -1,5 +1,7 @@
 package com.example.camera;
 
+import java.io.IOException;
+
 import android.os.Bundle;
 import android.app.Activity;
 import android.content.Intent;
@@ -27,10 +29,11 @@ public class Camera extends Activity implements View.OnClickListener {
 	}
 
 	private void initialize() {
-		iv = (ImageView) findViewById(R.id.Capture);
-		ib = (ImageButton) findViewById(R.id.bSetWall);
+		iv = (ImageView) findViewById(R.id.ivReturnedPic);
+		ib = (ImageButton) findViewById(R.id.ibTakePic);
+		b = (Button) findViewById(R.id.bSetWall);
 
-		iv.setOnClickListener(this);
+		b.setOnClickListener(this);
 		ib.setOnClickListener(this);
 	}
 
@@ -62,15 +65,22 @@ public class Camera extends Activity implements View.OnClickListener {
 
 	}
 
+	@SuppressWarnings("deprecation")
 	@Override
 	public void onClick(View v) {
 		// TODO Auto-generated method stub
 		switch (v.getId()) {
-		case R.id.Capture:
-
+		case R.id.bSetWall:
+			
+			try {
+				getApplicationContext().setWallpaper(bmp);
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
 			break;
 
-		case R.id.bSetWall:
+		case R.id.ibTakePic:
 
 			i = new Intent(android.provider.MediaStore.ACTION_IMAGE_CAPTURE);
 			startActivityForResult(i, cameraData);
